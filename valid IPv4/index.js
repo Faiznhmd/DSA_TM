b8df9afd7cbbf19ea1ad48692389c9907d4e1a9f
Faiznhmd <fiaznhmd.0624@gmail.com>
2024-05-16 12:05:44 +0530

    validIPv4

---
+++ valid IPv4/index.js
@@ -0,0 +1,11 @@
+function valdIPv4(input) {
+  const octets = input.split('.');
+
+  if (octets.length !== 4) {
+    return false;
+  }
+  return octets.every((octet) => {
+    const num = parseInt(octet);
+    return num >= 0 && num <= 255 && octet === num.toString();
+  });
+}
