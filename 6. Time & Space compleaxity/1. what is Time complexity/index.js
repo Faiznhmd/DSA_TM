e6abc0f25a522ac53624d16fe9cdd2b71df15b54
Faiznhmd <fiaznhmd.0624@gmail.com>
2024-05-18 22:32:29 +0530

    what is time complexity

---
+++ 6. Time & Space compleaxity/1. what is Time complexity/index.js
@@ -0,0 +1,11 @@
+//Runtime=Runtime is how much time a function take time to run
+
+//Types of Time Complexity
+
+//1. Constant= The Runtime remains same that how much the input is big
+
+//2. Linear=  As the input grow the Rumtime linearly grow
+
+//3. Quadratic= The quadratic of the input when the input is 1 the runtime became 10
+
+//4.Logarithmic
