14e2718eb56745f53af4f56447e99f20d663c4b3
Faiznhmd <fiaznhmd.0624@gmail.com>
2024-05-21 22:54:16 +0530

    hash table  basic

---
+++ 7.HashTable/1. Hash Table intro/index.js
@@ -0,0 +1,11 @@
+// Hash function is a function that takes in a key and return an index
+//Hash Collision= when two keys  maps in a same index
+
+//-------------common hash table-----------
+
+//Set(key)=Add key
+//get(key)=Return the value associated with that key
+// remove(key)=remove Key
+// has(key)=Check if a key exist
+// keys=return array of all keys in the hash table
+// values=return an array of all values in the hash table
