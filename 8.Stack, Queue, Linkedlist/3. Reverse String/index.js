05b768d84d07554f7a7a269a3c795c457e7f1913
Faiznhmd <fiaznhmd.0624@gmail.com>
2024-05-24 11:29:08 +0530

    reersed string

---
+++ 8.Stack, Queue, Linkedlist/3. Reverse String/index.js
@@ -0,0 +1,13 @@
+function reverseStringStack(str) {
+  const stack = new Stack();
+
+  for (let i = 0; i < str.length; i++) {
+    stack.push(str[i]);
+  }
+
+  let reverseString = '';
+  while (!stack.isEmpty()) {
+    reverseString += stack.pop();
+  }
+  return reverseString;
+}
