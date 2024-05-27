f22173435484bfbe63238b0335da3030a99afe6d
Faiznhmd <fiaznhmd.0624@gmail.com>
2024-05-27 22:30:32 +0530

    bubble sort

---
+++ RS-Sorting/index.js
@@ -0,0 +1,18 @@
+//Sort the given Array in Ascending order
+//[10,14,14,29,37]
+
+//Bubble Sort
+
+const BubbleSort = (arr) => {
+  let n = arr.length;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+};
+
+//selection sort
